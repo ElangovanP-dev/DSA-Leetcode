baf379250a3c97483a9099e6135eb22c22bc310c
Elangovan P <elangovanp222@gmail.com>
2026-04-08 19:22:33 +0530

    100Days of Coding Challenge

---
+++ Day-49/ValidParentheses.java
@@ -0,0 +1,20 @@
+public class ValidParentheses {
+    public static void main(String[] args) {
+        String s = "()";
+        System.out.println(isValid(s));
+    }
+
+    public static boolean isValid(String s) {
+        while (true) {
+            if (s.contains("()")) {
+                s = s.replace("()", "");
+            } else if (s.contains("{}")) {
+                s = s.replace("{}", "");
+            } else if (s.contains("[]")) {
+                s = s.replace("[]", "");
+            } else {
+                return s.isEmpty();
+            }
+        }
+    }
+}
