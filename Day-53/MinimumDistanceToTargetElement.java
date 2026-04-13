0406d1358f046aadf9d7cbdee61b3b029d82672d
Elangovan P <elangovanp222@gmail.com>
2026-04-13 17:09:21 +0530

    100Days of Coding Challenge

---
+++ Day-53/MinimumDistanceToTargetElement.java
@@ -0,0 +1,16 @@
+class MinimumDistanceToTargetElement {
+    public static int getMinDistance(int nums[], int target, int start) {
+        int minDis = Integer.MAX_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                minDis = Math.min(minDis, Math.abs(start - i));
+            }
+        }
+        return minDis;
+    }
+
+    public static void main(String args[]) {
+        int nums[] = { 1, 2, 3, 4, 5 };
+        System.out.println(getMinDistance(nums, 5, 3));
+    }
+}
